239f385ed8727086cbd743e6ab5ddf9afa9ec45d
Shaun Partridge <47838616+ShaunJPartridge@users.noreply.github.com>
2020-10-23 19:30:11 -0500

    Update check_Primes.py

--- Assignments/A06/check_Primes.py
+++ Assignments/A06/check_Primes.py
@@ -1,31 +1,112 @@
 import os
 import sys
 import math
+import pprint
 
-def checkPrimes(n):
+def mykwargs(argv):
+    '''
+    Processes argv list into plain args (list) and kwargs (dict).
+    Just easier than using a library like argparse for small things.
+    Example:
+        python file.py arg1 arg2 arg3=val1 arg4=val2 -arg5 -arg6 --arg7
+        Would create:
+            args[arg1, arg2, -arg5, -arg6, --arg7]
+            kargs{arg3 : val1, arg4 : val2}
+        Params with dashes (flags) can now be processed seperately
+    Shortfalls:
+        spaces between k=v would result in bad params
+        Flags aren't handled at all. Maybe in the future but this function
+            is meant to be simple.
+    Returns:
+        tuple  (args,kargs)
+    '''
+    args = []
+    kargs = {}
 
-    if n <= 1:
-        print(n,"Is not prime")
-        return False
-    if n <= 3:
-        print(n,"Is prime")
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        print(n,"Is not prime")
-        return False
-    i = 5
-    for i in range(i*i,n):
-        if n % i == 0 or n % (i+2) == 0:
-            print(n,"Is not prime")
-            return False      
-        i = i + 6
+    for arg in argv:
+        if '=' in arg:
+            key,val = arg.split('=')
+            kargs[key] = val
+        else:
+            args.append(arg)
+    return args,kargs
+
+def usage(message=None):
+    if message:
+        print(message)
+    print("Usage: python skeleton.py [key1=string] [key2=int] [key3=int] [keyX=sometype]")
+    print("Example:\n\t python skeleton.py arg1 arg2 var1='Param 1' path=./some_path a=25 b=50\n")
+    sys.exit()
+
+def isPrime(**kwargs):
+
+  input_f = kwargs.get('infile',None)
+
+  Values = []
+  infile = open(input_f,'r')
+  Values = [int(line) for line in infile.readlines()]
+
+
+  #with open(input_f,'r') as i:
+   # data = i.readlines()
+  for i in range(len(Values)):
+    N = Values[i]
+  
+  print(Values)
+
+  if N > 1: 
+      
+  # Iterate from 2 to n / 2  
+    for i in range(2, N): 
+         
+      # If num is divisible by any number between  
+      # 2 and n / 2, it is not prime  
+      if (N % i) == 0: 
+
+          # Call function PrimeFactors to get the prime factors of a number
+          PrimeFactors(N)
+          return False
+          
+          
+    else: 
+        print(N, "is a prime number")
+        return True 
+          
+  else: 
+    print(N, "is not a prime number")
+    return False   
+
+def PrimeFactors(n): 
     
-    print(n,"Is prime")
-    return True
+    # List to hold prime factors
+    Factors = []
+    # Print the number of two's that divide n and add to Factors
+    while n % 2 == 0: 
+        Factors.append(2)
+        print(2)
+        n = n / 2
+          
+    # n must be odd at this point 
+    # so a skip of 2 ( i = i + 2) can be used 
+    for i in range(3,int(math.sqrt(n))+1,2): 
+          
+        # while i divides n , print i and divide n and append i to Factors
+        while n % i== 0: 
+            Factors.append(i)
+            print(i)
+            n = n / i
+              
+    # Condition if n is a prime 
+    # number greater than 2 
+    if n > 2: 
+        print(int(n))
+    return Factors # return prime factors
     
 
-
+    
 if __name__ == "__main__":
 
-    checkPrimes(23)
+    #GetPrimes(72490732415291)
+    isPrime(72490732415291)
+    isPrime(3)
     
